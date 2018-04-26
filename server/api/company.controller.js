69aa538fe0b1d17f738209fab2817965bfb1a2ae
Neville <nevil_dabre@yahoo.com>
2018-04-26 09:55:21 -0400

    company REST

--- server/api/company.controller.js
+++ server/api/company.controller.js
@@ -1,10 +1,76 @@
 var express = require('express');
 var router = express.Router();
+var Company = require('../models/company');
 
-function index(req,res) {
-  res.send({message:'Company name'});
+function index(req, res) {
+  return Company.find()
+    .then(function (entity) {
+      res.send(entity)
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
+}
+
+function show(req, res) {
+  return Company.findById(req.param._id)
+    .then(function (entity) {
+      res.send(entity);
+    })
+    .catch(function (err) {
+      console.log(err);
+    })
+}
+
+function create(req, res) {
+  var newCompany = Company({
+    name : req.body.name,
+    address: req.body.address
+  })  
+
+  return newCompany.save()
+    .then(function(entity){
+      res.send(entity);
+    })
+    .catch(function(err){
+      console.log(err);
+    })
+}
+
+function update(req, res){
+  var _id = req.param._id;
+
+  var entry = {
+    name : req.body.name,
+    address: req.body.address
   }
 
-router.get('/', index)
+  return Company.findByIdAndUpdate(_id, entry)
+    .then(function(entity){
+      res.send(entity);
+    })
+    .catch(function(err){
+      console.log(err);
+    })
+}
+
+function destroy(req, res){
+  var _id = req.param._id;
+
+  return Company.findByIdAndRemove(_id)
+    .then(function(entity){
+      res.send(entity);
+    })
+    .catch(function(err){
+      console.log(err);
+    })
+}
+
+router.get('/', index);
+router.get('/:id', show);
+router.post('/', create);
+router.put('/:id', update);
+router.delete('/:id', destroy);
+
 
 module.exports = router;
